79456f98d5c1b0ca3b8e33d95f286c88cf24d72e
Shivani Agarwal <77854814+shivani-1198@users.noreply.github.com>
2024-02-06 14:31:02 -0500

    Add files via upload

---
+++ src/Servies.js/GlobalApi.jsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+const key = "1f7bfc4bc49f42b2b78ac682c30c5401";
+const axioCreate = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  // params: {
+  //   key: "18d8dc115d954615a6fe8522598e8a97",
+  // },
+});
+
+const getPopularGame = axioCreate.get("/games?key=" + key);
+const getMovieDetails = (id) => axioCreate.get("/games/" + id + "/movies");
+const getGameListByGenreId = (id) =>
+  axioCreate.get("/games?key=" + key + "&genres=" + id);
+export default {
+  getPopularGame,
+  getMovieDetails,
+  getGameListByGenreId,
+};
